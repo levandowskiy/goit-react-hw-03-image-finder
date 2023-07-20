478689c8014651333bd969dc23f46fd98c532dd3
Dima Levchenko <112283201+levandowskiy@users.noreply.github.com>
2023-07-20 14:35:26 +0300

    u

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,66 +1,22 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Modal from './Modal';
-import fetchImg from '../service/pixabay-api'; 
 
 class App extends Component {
   state = {
     searchValue: '',
     visibility: false,
     modalImg: '',
-    searchResult: null,
-    totalResult: 0,
-    isLoading: false,
-    errorMessage: '',
   };
 
-  handlerFormSubmit = (searchValue) => {
+  handlerFormSubmit = searchValue => {
     this.setState({
       searchValue,
     });
-
-    this.fetchImageData(searchValue, 1);
   };
 
-  fetchImageData = (searchValue, page) => {
-    this.setState({
-      isLoading: true,
-      searchResult: null,
-      errorMessage: '',
-    });
-
-    fetchImg(searchValue, page)
-      .then((searchResult) => {
-        if (searchResult.hits.length === 0) {
-          this.setState({
-            searchResult: null,
-            totalResult: 0,
-            errorMessage: 'Запит не знайдено',
-          });
-        } else {
-          this.setState({
-            searchResult: searchResult.hits,
-            totalResult: searchResult.totalHits,
-          });
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        this.setState({
-          searchResult: null,
-          totalResult: 0,
-          errorMessage: 'Помилка під час виконання запиту',
-        });
-      })
-      .finally(() => {
-        this.setState({
-          isLoading: false,
-        });
-      });
-  };
-
-  handlerOpenModal = (largeImg) => {
+  handlerOpenModal = largeImg => {
     this.setState({
       visibility: true,
       modalImg: largeImg,
@@ -73,37 +29,18 @@ class App extends Component {
     });
   };
 
-  _handlerLoadMore = async () => {
-    await this.setState((prevState) => ({
-      page: prevState.page + 1,
-      isLoading: true,
-    }));
-
-    const { page, searchValue } = this.state;
-
-    this.fetchImageData(searchValue, page);
-  };
-
   render() {
-    const { visibility, searchValue, modalImg, searchResult, totalResult, isLoading, errorMessage } = this.state;
+    const {visibility, searchValue, modalImg, } = this.state;
     return (
       <>
         <Searchbar handlerFormSubmit={this.handlerFormSubmit} />
-        <ImageGallery
-          handlerOpenModal={this.handlerOpenModal}
-          searchValue={searchValue}
-          searchResult={searchResult}
-          totalResult={totalResult}
-          isLoading={isLoading}
-          errorMessage={errorMessage}
-          handlerLoadMore={this._handlerLoadMore}
-        />
+        <ImageGallery handlerOpenModal={this.handlerOpenModal} searchValue={searchValue} />
         {visibility && (
-          <Modal
-            largeImg={modalImg}
-            handlerCloseModal={this.handlerCloseModal}
-          />
-        )}
+            <Modal
+              largeImg={modalImg}
+              handlerCloseModal={this.handlerCloseModal}
+            />
+         )}
       </>
     );
   }
